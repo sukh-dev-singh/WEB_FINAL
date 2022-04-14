d873b3cc4837138cd1b1a668da119118124388bc
sukh-dev-singh <103700173+sukh-dev-singh@users.noreply.github.com>
2022-04-14 19:16:31 -0400

    Signed-off-by: sukh-dev-singh <103700173+sukh-dev-singh@users.noreply.github.com>

---
+++ web.js
@@ -0,0 +1,12 @@
+function bigImg(x) {
+    x.style.height = "425px";
+    x.style.width = "425px";
+  }
+  
+  function normalImg(x) {
+    x.style.height = "400px";
+    x.style.width = "400px";
+  }          
+        
+  
+    
